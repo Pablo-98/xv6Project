b20c7d667b2e32584a8c0209b01484a41c9233b2
seed <hpablo575@gmail.com>
2025-09-21 20:55:10 +0000

    create uptime.c in usr

---
+++ user/uptime.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+
+
+void myUptime() {
+ fprint("uptime: %d ", uptime() );
+ exit(0);
+}
+
+/*See kernel/sysproc.c for the kernel code that implements the uptime system call
+(look for sys uptime), user/user.h for the C definition of uptime callable from a user
+3program, and user/usys.S for the assembler code that jumps from user code into the
+kernel for uptime.
+*/
